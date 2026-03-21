39c6612fd40ec7e62f9581fd90349d75558e8a3a
Erick Gist II <erick@erickgist.com>
2026-03-20 18:20:52 -0700

    feat:v2 setup eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,13 +20,36 @@ export default [
         Utilities: "readonly",
         PropertiesService: "readonly",
         ScriptApp: "readonly",
+        CardService: "readonly",
+        Tasks: "readonly",
         console: "readonly",
+
+        // --- Add your Custom Global Functions here ---
+        // If Code.js calls a function defined in api.js, 
+        // add the function name here so ESLint knows it exists.
+        fetchAllTaskLists: "writable",
+        buildDashboardCard: "writable",
+        buildTaskCard: "writable",
+        fetchAndLinkTasks: "writable",
+        fetchExistingProjects: "writable",
+        getWorkspaceUsers: "writable",
+        insertTask: "writable",
+        createElevatedTask: "writable",
+        getTask: "writable",
+        parseMetadata: "writable",
+        updateTaskData: "writable",
+        buildElevatedNotes: "writable",
+        getCleanNotes: "writable",
       },
     },
     rules: {
-      "no-unused-vars": "warn",   // Highlights variables you defined but didn't use
+      "no-unused-vars": ["warn", {
+        "vars": "local",
+        "args": "none"
+      }],  // Highlights variables you defined but didn't use
       "no-undef": "error",        // Stops the build if you use an unknown variable (typo)
       "no-extra-semi": "error",   // Keeps the code clean from double semicolons
+      "no-redeclare": "off"
     },
   },
-];
+]; 
